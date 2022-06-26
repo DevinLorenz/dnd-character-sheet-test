8f85a4c07c81334e42be4a07cb5c66363d4524a0
Devin Lorenz <devinlorenz.work@gmail.com>
2022-06-26 17:23:51 -0400

    Made AC able to change based on if Armor is in equipped array

--- inventory-system.js
+++ inventory-system.js
@@ -1,5 +1,4 @@
-//I am going to make arrays that correspond with three different inventory spaces: Equipped, Backpack, Satchel.
-
+//Let's make a d&d Character Sheet!
 let heroName = `Vaal'Vahn`
 const characterClass = 'Fighter'
 let level = 5
@@ -13,8 +12,13 @@ const intellegence = 1
 const wisdom = 2
 const charisma = -1
 
+let halfPlate = 5
+
+let proficiencyBonus = 3
 const initiative = dexterity
-const armorClass = 10 + dexterity + 5
+
+
+
 
 
 let backPack = [];
@@ -24,11 +28,48 @@ let equipped = [];
 let clothes = `Traveller's Clothes`
 let rations = 'Food Rations'
 let weapon = 'Longsword'
-let potion = 'Potion of Greater Healing'
+let hPotion = 'Potion of Greater Healing'
 let armor = `Half-Plate Armor`
 let amulet = `Amulet of Osiris`
 
-//Logging character information.
+let armorClass = 10 + dexterity
+
+
+
+
+
+
+
+let armorEquip = true
+armorEquip = false
+armorEquip = true
+
+equipped.push(weapon,amulet,clothes)
+
+if (armorEquip === true){
+    equipped.unshift(armor)
+} else if (armorEquip === false) {
+    equipped.shift(armor)
+}
+
+
+
+if (armor === equipped[0]){
+    armorClass += halfPlate
+} else if (armor !== equipped[0]){
+    armorClass = (10 + dexterity)
+}
+
+
+
+
+
+
+
+
+
+
+    //Logging character information.
 console.log()
 
 console.log(`Character: ${heroName}`)
@@ -53,35 +94,73 @@ console.log(`     Charisma: 9 | ${charisma}`)
 console.log()
 console.log()
 
-console.log(`Initiative: +${initiative}    AC:${armorClass}`)
+console.log(` Proficiency Bonus: +${proficiencyBonus}`)
+console.log(`Initiative: +${initiative}    AC: ${armorClass}`)
 
 console.log()
-    console.log(`----------------------------------------`)
+console.log(`----------------------------------------`)
+console.log()
 
-//good. now lets push potions and rations to backpack.
 
-backPack.push(rations , potion)
 
-//Lets create a line that says how much space is being taken up.
+
+
+
+//Now lets push hPotion and rations to backpack.
+
+backPack.push(rations , hPotion)
+
+//Lets create a line that says how much space is being taken up in the backpack. Let's make the backpack only have space for 10 items.
 
 let backPackSlots = backPack.length
 
-//Lets console log the items in the backpack and how many items out of 10 there are.
-console.log()
+//log the items in the backpack and how many items out of 10 there are.
 
 console.log(`Backpack inventory: ${backPack}`)
     console.log(`Space: ${backPackSlots}/10`)
-
-//Now lets equip some items.
-
-equipped.push(weapon,amulet,armor,clothes)
-
-let equippedSlots = equipped.length
-
-console.log()
-
-console.log(`Equipped: ${equipped}`)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Now lets equip some items.
+    
+    
+    
+    let equippedSlots = equipped.length
+    
+   
+    console.log()
+    
+    console.log(`Equipped: ${equipped}`)
     console.log(`Space: ${equippedSlots}/6`)
+
+    console.log()
+    console.log(`----------------------------------------`)
+    console.log()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 console.log()
 console.log(`----------------------------------------`)
@@ -90,18 +169,18 @@ console.log()
 console.log(`You Attack! Roll to hit!`)
 console.log()
 
-let longSwordDamage = Math.floor((Math.random() * 10) + 1) + 5;
-let longSwordAttack = Math.floor((Math.random() * 20) + 1) + 8
+let longSwordDamage = Math.floor((Math.random() * 10) + 1) + strength;
+let longSwordAttack = Math.floor(((Math.random() * 20) + 1) + strength) + proficiencyBonus
 
 if((longSwordAttack - 8) <= 1){
     console.log('Critical Failure!')
     console.log(`You fumble and drop your sword!`)   
-} else if (longSwordAttack <= 14){ 
-    console.log(`You swing your sword for a ${longSwordAttack} and miss!`)
+} else if (longSwordAttack < 14){ 
+    console.log(`You roll a ${longSwordAttack} to swing your sword and miss!`)
 } else if(longSwordAttack > 27){
     console.log('Critical Hit!')
     console.log(`You do ${longSwordDamage * 2 - 5} points of critical slashing damage!`)
-} else {    console.log(`You swing your sword for a ${longSwordAttack} to hit!`)
+} else {    console.log(`You swing your sword with a ${longSwordAttack} to hit!`)
             console.log(`You do ${longSwordDamage} points of slashing damage!`)  
 }
 
